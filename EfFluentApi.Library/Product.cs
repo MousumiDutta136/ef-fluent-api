6c2e66e8a2b38de6c47340708153b1df77b1eb1d
Milad Ghafoori <m_ghafoori@hotmail.com>
2021-07-25 21:03:43 -0400

    Update Product.cs

--- EfFluentApi.Library/Product.cs
+++ EfFluentApi.Library/Product.cs
@@ -9,7 +9,7 @@ namespace EfFluentApi.Library
         public string Description { get; set; }
 
         public int CategoryId { get; set; }                 // <-- Use for one to many and one to one relationships
-        public Category Category { get; set; }      // <-- - Use for one to many and one to one relationships
+        public Category Category { get; set; }              // <-- - Use for one to many and one to one relationships
                                                             //     - Make virtual for lazy loading
 
         // public IList<Category> Categories { get; set; }  // <-- Use for many to many relationships
